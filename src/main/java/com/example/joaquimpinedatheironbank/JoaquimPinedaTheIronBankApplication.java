44070d5c446a39fe55d8b086fdaa57ada5d2d4d4
Joaquim Pinda <joaquimpineda@gmail.com>
2022-09-06 21:51:38 +0200

    initial commit

---
+++ src/main/java/com/example/joaquimpinedatheironbank/JoaquimPinedaTheIronBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.joaquimpinedatheironbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoaquimPinedaTheIronBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoaquimPinedaTheIronBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/joaquimpinedatheironbank/JoaquimPinedaTheIronBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joaquimpinedatheironbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoaquimPinedaTheIronBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
